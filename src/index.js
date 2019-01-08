4d6badb99e1e5e71cdeb8f77f742596270477f3d
JakubKarp <jakub.karp@innespacery.pl>
2019-01-08 22:47:35 +0100

    first push

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+const App = () => {
+    
+
+    return <div >
+        <h1 > To niesamowite!</h1>
+        <p>Działa :</p> 
+    </div>
+}
+
+render ( 
+    <App /> ,
+    document.getElementById('root')
+)
